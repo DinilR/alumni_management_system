5e9357b2102ae6899c55c6640f1b1fc9574409d2
DinilR <dinilsratnayake@gmail.com>
2023-04-11 00:36:50 +0530

    Initial Commit

---
+++ admin/db_connect.php
@@ -0,0 +1,4 @@
+<?php 
+
+$conn= new mysqli('localhost','root','','stc')or die("Could not connect to mysql".mysqli_error($con));
+// $conn = new mysqli('134.209.183.166', 'stc_user', 'ST)_u$r@#21', 'stc') or die("Could not connect to mysql".mysqli_error($con));
